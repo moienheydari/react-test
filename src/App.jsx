9cff1f79c32b66048f961f50ae7e3e183adf8db2
Moien <137896500+moienheydari@users.noreply.github.com>
2023-08-24 23:03:29 +0330

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,12 @@ import Navigation from './assets/Slide-out-Navigation/Navigation.jsx';
 import Social from './assets/Social-Button/Social.jsx';
 import background from './assets/background.svg';
 import MyForm from './assets/Formik-Form/Form.jsx';
-// import Delayed from './assets/Delayed-Component/Delayed.jsx';
+import LoadingSpinner from './assets/Loading-Spinner/LoadingSpinner';
 
 const Delayed = import('./assets/Delayed-Component/Delayed.jsx');
 
 
 export default function App() {
-  // const Delayed = React.lazy(() => { import('./assets/Delayed-Component/Delayed.jsx') });
-  // const Delayed = import('./assets/Delayed-Component/Delayed');
 
   return (
     <div className='App' style={{
@@ -21,24 +19,30 @@ export default function App() {
       backgroundSize: 'cover'
     }}>
       <div className='things'>
+
         <div className='grid-element navigation'>
           <Navigation />
         </div>
+
         <div className='grid-element social' >
           <Social />
         </div>
+
         <div className='grid-element dothover' >
           <Dothoverbutt />
         </div>
+
         <div className='grid-element rotate' >
           <div className='front'></div>
           <div className='back'></div>
         </div>
+
         <div className='grid-element button-boxshadow' >
           <button className='button1'>Click!</button>
           <button className='button2'>Click!</button>
           <button className='button3'>Click!</button>
         </div>
+
         <div className='grid-element' >
           <div className='cool-input'>
             <input type='text' className='input-field' id='inp' placeholder='Name'>
@@ -48,14 +52,15 @@ export default function App() {
             </label>
           </div>
         </div>
+
         <div className='grid-element form' >
           <MyForm />
         </div>
-        <div className='grid-element delayed' >
-          {/* <Suspense fallback={<h1>Loading....</h1>}> */}
-            <Delayed />
-          {/* </Suspense> */}
+        
+        <div className='grid-element loader'>
+          <LoadingSpinner />
         </div>
+
       </div>
     </div>
   )
---
+++ src/assets/Loading-Spinner/LoadingSpinner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+export default function LoadingSpinner() {
+    return (
+        <div class="spinner">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
