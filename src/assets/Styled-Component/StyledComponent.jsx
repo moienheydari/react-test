3b016320b453c8004590d7626b1df6579cd70cfa
Moien <137896500+moienheydari@users.noreply.github.com>
2023-09-12 19:49:44 +0330

    full styled component

--- src/assets/Styled-Component/StyledComponent.jsx
+++ src/assets/Styled-Component/StyledComponent.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import styled, { ThemeProvider, createGlobalStyle, keyframes } from 'styled-components';
+
+const theme = {
+    fontFamily: 'Segoe UI'
+}
 
 export default function StyledComponent() {
+    const [name, setName] = useState('');
+    const [pass, setPass] = useState('');
+
     return (
-        <Cont>
-            <Text color='red'>Hi to </Text>
-            <Text>Styled Component</Text>
-        </Cont>
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Cont>
+                <Text color='red'>Hi to </Text>
+                <Text>Styled Component</Text>
+                <SubText color='black'>Amazing!</SubText>
+                <Input name='name' value={name} setter={setName}></Input>
+                <Input type='password' name='pass' value={pass} setter={setPass}></Input>
+                <RotatingButton>YO!</RotatingButton>
+            </Cont>
+        </ThemeProvider>
     )
 }
+
+const GlobalStyle = createGlobalStyle`
+    h1 {
+        font-family: ${(props) => props.theme.fontFamily};
+    }
+`
 
 const Cont = styled.div`
     display: grid;
@@ -19,4 +39,55 @@ const Text = styled.h1`
     font-size: larger;
     color: ${(props) => props.color || 'aqua'} ;
     font-weight: bold;
+    transition: 0.3s;
+    &:hover {
+        transform: scale(1.1);
+    }
 `
+
+const SubText = styled(Text)`
+    background-image: linear-gradient(to right, #fff, #ff0);
+    padding: 8px;
+    padding-bottom: 10px;
+    border-radius: 3px;
+    height: auto;
+`
+
+const Input = styled.input.attrs((props) => ({
+    placeholder: 'Type here...',
+    onChange: ({ target }) => { props.setter(target.value) }
+}))`
+    padding: 2px;
+    background-color: teal;
+`
+
+
+const rotate = keyframes`
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+`
+
+const RotatingButton = styled.button`
+    margin-top: 10px;
+    height: 40px;
+    width: 40px;
+    background-color: aliceblue;
+    box-shadow: 2px 2px yellowgreen;
+    transition: 0.2s;
+    border-radius: 40%;
+
+    animation: ${rotate} infinite 2s linear;
+
+    &:hover {
+        transform: scale(1.1);
+    }
+
+    &:active {
+        transform: scale(0.9);
+    }
+`
+
